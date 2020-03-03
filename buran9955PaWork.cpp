d76217f368dbc4c90a541b640baa5d7d852059dd
Uran <nobady@mail.net>
2020-03-03 16:27:35 +0300

    ok

--- buran9955PaWork.cpp
+++ buran9955PaWork.cpp
@@ -4,20 +4,21 @@
 #pragma hdrstop
 #include <tchar.h>
 //---------------------------------------------------------------------------
-USEFORM("unMain.cpp", fmMain);
 USEFORM("unSetDirectory.cpp", fmSetDirectory);
-USEFORM("unFmViewZone.cpp", fmViewZone);
-USEFORM("unFmReport.cpp", fmReport);
-USEFORM("unFmShowZones.cpp", fmShowZones);
 USEFORM("unShowListTubes.cpp", fmShowListTubes);
+USEFORM("unMain.cpp", fmMain);
+USEFORM("unFmViewZone.cpp", fmViewZone);
+USEFORM("Unit1.cpp", Form1);
 USEFORM("unSQLDbModule.cpp", SqlDBModule); /* TDataModule: File Type */
-USEFORM("unFmModifyRecord.cpp", fmModifyRecord);
+USEFORM("unFmShowZones.cpp", fmShowZones);
+USEFORM("unFmBackup.cpp", fmCreateBackup);
 USEFORM("ABOUT_NTC_NK_URAN.cpp", fmAboutBox);
-USEFORM("QueryMessageForm.cpp", MessageForm);
+USEFORM("unFmModifyRecord.cpp", fmModifyRecord);
+USEFORM("unFmReport.cpp", fmReport);
+USEFORM("unFmGSettings.cpp", fmGSettings);
 USEFORM("unFmInfo.cpp", fmInfo);
 USEFORM("unFmMessage.cpp", fmMessage);
-USEFORM("unFmGSettings.cpp", fmGSettings);
-USEFORM("unFmBackup.cpp", fmCreateBackup);
+USEFORM("QueryMessageForm.cpp", MessageForm);
 //---------------------------------------------------------------------------
 int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
@@ -28,6 +29,7 @@ int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 		Application->CreateForm(__classid(TSqlDBModule), &SqlDBModule);
 		Application->CreateForm(__classid(TfmMain), &fmMain);
 		Application->CreateForm(__classid(TfmModifyRecord), &fmModifyRecord);
+		Application->CreateForm(__classid(TfmMessage), &fmMessage);
 		Application->Run();
 	}
 	catch (Exception &exception)
